cd5cac3f5458abd2e9729fc979baec106d72afe2
BIRALO256 <jovic@gmail.com>
2026-07-21 19:18:02 +0300

    chore: scaffold monorepo structure with apps and shared packages

--- apps/rider_app/test/widget_test.dart
+++ apps/rider_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:_24boda/main.dart';
+import 'package:rider_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
